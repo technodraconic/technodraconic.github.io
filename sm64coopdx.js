a1d8c2346506e940812fc438750ff7bc240e9b28
TechnoDraconic <riandoty@gmail.com>
2025-12-31 16:36:07 -0800

    new localtunnel link

--- sm64coopdx.js
+++ sm64coopdx.js
@@ -4582,7 +4582,7 @@ async function createWasm() {
             try {
               // The default value is 'ws://' the replace is needed because the compiler replaces '//' comments with '#'
               // comments without checking context, so we'd end up with ws:#, the replace swaps the '#' for '//' again.
-              var url = 'wss://gentle-moose-smash.loca.lt'.replace('#', '//');
+              var url = 'wss://early-snails-beam.loca.lt'.replace('#', '//');
               // Make the WebSocket subprotocol (Sec-WebSocket-Protocol) default to binary if no configuration is set.
               var subProtocols = 'binary'; // The default value is 'binary'
               // The default WebSocket options
